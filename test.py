808d64133e25a4ef763ac2cf5d364e9efdc6b294
hunterone3 <58080776+hunterone3@users.noreply.github.com>
2020-12-28 14:44:30 +0200

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,11 @@ class MyTest(unittest.TestCase):
         
         
 if __name__ == "__main__":
+    import xmlrunner
+    runner=xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
     unittest.main()
+    
 
 
 
