fdc5d0188fe77eacf6858a480f44457477f71ae3
hunterone3 <58080776+hunterone3@users.noreply.github.com>
2020-12-28 14:39:04 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+
+import copy
+import unittest
+
+def sum(a0, d, n):
+    if n == 1:
+        return a0
+    else:
+        return a0 + (n-1) * d + sum(a0, d, n-1)
+
+print('Сума прогресії: ') 
+print (sum(1, 2, 3))
+
+class MyTest(unittest.TestCase): 
+    def test_usage1(self):
+        self.assertIsNot(3, sum(1, 2, 3))   
+        
+        
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
